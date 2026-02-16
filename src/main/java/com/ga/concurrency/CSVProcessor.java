e128666cd09f154b83c5e2dd5509e181fe2a0268
Zahralsayed <zalmatooq31@gmail.com>
2026-02-17 01:34:21 +0300

    method to map to employee (separate lines)

--- src/main/java/com/ga/concurrency/CSVProcessor.java
+++ src/main/java/com/ga/concurrency/CSVProcessor.java
@@ -1,7 +1,32 @@
 package com.ga.concurrency;
 
+import com.ga.concurrency.model.Employee;
+import com.ga.concurrency.model.Role;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Component
 public class CSVProcessor {
+
+
+
+    private Employee mapToEmployee(String line) {
+        String[] parts = line.split(",");
+
+        return new Employee(
+                parts[1],
+                Double.parseDouble(parts[2]),
+                LocalDate.parse(parts[3]),
+                Role.valueOf(parts[4].toUpperCase()),
+                Double.parseDouble(parts[5])
+        );
+    }
+
 }
