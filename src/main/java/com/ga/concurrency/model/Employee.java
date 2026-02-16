5e308c3653d18aad2f008ad22b407761f0f23bc9
Zahralsayed <zalmatooq31@gmail.com>
2026-02-17 01:19:23 +0300

    add Employee model

---
+++ src/main/java/com/ga/concurrency/model/Employee.java
@@ -0,0 +1,11 @@
+package com.ga.concurrency.model;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private LocalDate joinedDate;
+    private Role role;
+    private double projectCompletionPercentage;
+}
